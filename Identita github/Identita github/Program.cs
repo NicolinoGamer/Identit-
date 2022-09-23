e6691327b74ebd03e03df592dacfeb6acabb8627
NicolinoGamer <111190641+NicolinoGamer@users.noreply.github.com>
2022-09-23 19:49:05 +0200

    Add files via upload

---
+++ Identita github/Identita github/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Identita_github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Anni = "";
+            string nascita = "";
+            string anno = "";
+
+
+            Console.WriteLine("Carta di identita");
+            Console.WriteLine("Hai" + Anni);
+            Console.WriteLine("Sei nato il" + nascita);
+            Console.WriteLine("Del" + anno);
+            Console.ReadKey();
+        }
+    }
+}
